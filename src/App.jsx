04d4e332faf7b98523b553d9c5a84f05d6f3a8de
a8hayyy <acuber1728@gmail.com>
2026-06-27 00:52:27 +0530

    Initial commit: Launching soon page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1 className="title">solodev.in</h1>
+        <p className="subtitle">System booting... Projects compiling...</p>
+        
+        <a href="mailto:hello@solodev.in" className="pixel-btn">
+          Get in Touch
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default App
